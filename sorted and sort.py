befe842c226c920bcfbb0eb93db5fbddebf08655
Afifa1404 <afifak886@gmail.com>
2026-05-21 15:11:18 +0530

    Add files via upload

---
+++ sorted and sort.py
@@ -0,0 +1,8 @@
+lang=['java','c','python','cpp','sq1','SQl']
+lang2=sorted(lang)
+print("The sorted list lang2 is:", lang2)
+print("The sorted list lang2 is:",sorted(lang))
+print("The orginal list lang is:", lang)
+print("The sort list lang returning None:",lang.sort())
+print("The sorted list lang2 in descending order using sorted is :",sorted(lang,reverse=True))
+print("The orginal updated sorted list(descending order)using sort function is",lang)
