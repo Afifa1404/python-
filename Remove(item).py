d39e591ae0bb5e0e9e0363d56e7a6ed7c588d4de
Afifa1404 <afifak886@gmail.com>
2026-05-21 15:08:25 +0530

    Add files via upload

---
+++ Remove(item).py
@@ -0,0 +1,6 @@
+months={"Jan","Feb","Mar","Apr"}
+print("\nPrinting the original set...",months)
+months.remove("Apr")
+print("\nPrinting the modified set...",months)
+months.remove("Mar")
+print("\nPrinting the modified set...",months)
