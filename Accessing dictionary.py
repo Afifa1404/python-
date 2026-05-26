61396a833044d3c1c75eb35bce41ee80c8f00283
Afifa1404 <afifak886@gmail.com>
2026-05-26 11:18:13 +0530

    Add files via upload

---
+++ Accessing dictionary.py
@@ -0,0 +1,5 @@
+student={"Name":"Afifa","Age":19,"Regno":162,"Branch":"CSE"}
+print("Name:",student["Name"])
+print("Age:",student["Age"])
+print("Regno:",student["Regno"])
+print("Branch:",student["Branch"])
